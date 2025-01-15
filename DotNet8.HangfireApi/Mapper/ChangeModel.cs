38268e3610088d076b42d4769fe396a5ec3d2cb1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 18:00:22 +0630

    Feat : Add BlogModel Map in Change Model

--- DotNet8.HangfireApi/Mapper/ChangeModel.cs
+++ DotNet8.HangfireApi/Mapper/ChangeModel.cs
@@ -1,6 +1,20 @@
-﻿namespace DotNet8.HangfireApi.Mapper
+﻿using DotNet8.HangfireApi.Features.Blog;
+using DotNet8.HangfireApi.Models;
+using DotNet8.HangfireApi.Modelts;
+
+namespace DotNet8.HangfireApi.Mapper
 {
-    public class ChangeModel
+    public static class ChangeModel
     {
+        public static BlogModel Map(this TblBlog dataModel)
+        {
+            return new BlogModel
+            {
+                BlogId = dataModel.BlogId,
+                BlogTitle = dataModel.BlogTitle,
+                BlogAuthor = dataModel.BlogAuthor,
+                BlogContent = dataModel.BlogContent
+            };
+        }
     }
 }
