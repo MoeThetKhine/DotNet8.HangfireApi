7571dccd2430f5612fea1ee1eb2118d87fa4d4e1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 15:33:15 +0630

    Format Code

--- DotNet8.HangfireApi/Models/BlogRequestModel.cs
+++ DotNet8.HangfireApi/Models/BlogRequestModel.cs
@@ -1,9 +1,8 @@
-﻿namespace DotNet8.HangfireApi.Models
+﻿namespace DotNet8.HangfireApi.Models;
+
+public class BlogRequestModel
 {
-    public class BlogRequestModel
-    {
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
