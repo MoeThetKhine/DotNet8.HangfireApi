9904d3a5fce897ae6bfaded94d02fa1e9ae8bf2b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 19:25:43 +0630

    Feat : Add CreateBlog in IBlogRepository

--- DotNet8.HangfireApi/Repositories/Blog/IBlogRepository.cs
+++ DotNet8.HangfireApi/Repositories/Blog/IBlogRepository.cs
@@ -5,5 +5,6 @@ namespace DotNet8.HangfireApi.Repositories.Blog
     public interface IBlogRepository
     {
         Task<BlogListResponseModel> GetBlogs();
+        Task<int> CreateBlog(BlogRequestModel requestModel);
     }
 }
