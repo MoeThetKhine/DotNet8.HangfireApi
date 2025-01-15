dca30211a9d28f58929207ab5fc9a9d19daebce8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 21:51:28 +0630

    Format Code

--- DotNet8.HangfireApi/Mapper/ChangeModel.cs
+++ DotNet8.HangfireApi/Mapper/ChangeModel.cs
@@ -1,26 +1,25 @@
-﻿namespace DotNet8.HangfireApi.Mapper
-{
-    public static class ChangeModel
-    {
-        public static BlogModel Map(this TblBlog dataModel)
-        {
-            return new BlogModel
-            {
-                BlogId = dataModel.BlogId,
-                BlogTitle = dataModel.BlogTitle,
-                BlogAuthor = dataModel.BlogAuthor,
-                BlogContent = dataModel.BlogContent
-            };
-        }
+﻿namespace DotNet8.HangfireApi.Mapper;
 
-        public static TblBlog Map(this BlogRequestModel requestModel)
+public static class ChangeModel
+{
+    public static BlogModel Map(this TblBlog dataModel)
+    {
+        return new BlogModel
         {
-            return new TblBlog
-            {
-                BlogTitle = requestModel.BlogTitle,
-                BlogAuthor = requestModel.BlogAuthor,
-                BlogContent = requestModel.BlogContent
-            };
-        }
+            BlogId = dataModel.BlogId,
+            BlogTitle = dataModel.BlogTitle,
+            BlogAuthor = dataModel.BlogAuthor,
+            BlogContent = dataModel.BlogContent
+        };
+    }
+
+    public static TblBlog Map(this BlogRequestModel requestModel)
+    {
+        return new TblBlog
+        {
+            BlogTitle = requestModel.BlogTitle,
+            BlogAuthor = requestModel.BlogAuthor,
+            BlogContent = requestModel.BlogContent
+        };
     }
 }
