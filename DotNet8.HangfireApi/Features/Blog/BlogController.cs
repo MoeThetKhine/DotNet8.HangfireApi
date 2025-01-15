187cc8c3ad3e625b7c140fc643e6e9fc9c0997a6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 18:50:55 +0630

    Feat : Add GetBlog in Blog Controller

--- DotNet8.HangfireApi/Features/Blog/BlogController.cs
+++ DotNet8.HangfireApi/Features/Blog/BlogController.cs
@@ -5,7 +5,27 @@ namespace DotNet8.HangfireApi.Features.Blog
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BlogController : ControllerBase
+    public class BlogController : BaseController
     {
+        private readonly BL_Blog _blBlog;
+
+        public BlogController(BL_Blog blBlog)
+        {
+            _blBlog = blBlog;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogs()
+        {
+            try
+            {
+                var result = await _blBlog.GetBlogs();
+                return Content(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
     }
 }
