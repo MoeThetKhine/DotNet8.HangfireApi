cb5ef207b077029687f07e95a752ac55bd043ce5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 18:50:32 +0630

    Feat : Add GetBlogs in BL_Blog

--- DotNet8.HangfireApi/Features/Blog/BL_Blog.cs
+++ DotNet8.HangfireApi/Features/Blog/BL_Blog.cs
@@ -1,6 +1,20 @@
-﻿namespace DotNet8.HangfireApi.Features.Blog
+﻿using DotNet8.HangfireApi.Models;
+using DotNet8.HangfireApi.Repositories.Blog;
+
+namespace DotNet8.HangfireApi.Features.Blog
 {
     public class BL_Blog
     {
+        private readonly IBlogRepository _blogRepository;
+
+        public BL_Blog(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<BlogListResponseModel> GetBlogs()
+        {
+            return await _blogRepository.GetBlogs();
+        }
     }
 }
