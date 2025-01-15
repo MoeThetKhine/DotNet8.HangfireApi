21b327f636124a99568fb4922133a5bd445dd995
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 21:49:15 +0630

    -

--- DotNet8.HangfireApi/Repositories/Blog/IBlogRepository.cs
+++ DotNet8.HangfireApi/Repositories/Blog/IBlogRepository.cs
@@ -1,7 +1,11 @@
 ﻿namespace DotNet8.HangfireApi.Repositories.Blog;
 
+#region IBlogRepository
+
 public interface IBlogRepository
 {
     Task<BlogListResponseModel> GetBlogs();
     Task<int> CreateBlog(BlogRequestModel requestModel);
 }
+
+#endregion
