d827bdf578507427200175678311e0b0ca348fc5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 15:31:34 +0630

    Feat : Add BlogRequestModel

--- DotNet8.HangfireApi/Models/BlogRequestModel.cs
+++ DotNet8.HangfireApi/Models/BlogRequestModel.cs
@@ -2,5 +2,8 @@
 {
     public class BlogRequestModel
     {
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
     }
 }
