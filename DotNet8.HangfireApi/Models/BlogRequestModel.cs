90abee4f556085db997847412df31fa3465f4c15
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 15:33:32 +0630

    -

--- DotNet8.HangfireApi/Models/BlogRequestModel.cs
+++ DotNet8.HangfireApi/Models/BlogRequestModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace DotNet8.HangfireApi.Models;
 
+#region BlogRequestModel
+
 public class BlogRequestModel
 {
     public string BlogTitle { get; set; }
     public string BlogAuthor { get; set; }
     public string BlogContent { get; set; }
 }
+
+#endregion
