b71f3644cf0a0b3d98ff915f01bd1f657485eb51
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 21:48:03 +0630

    Format Code

--- DotNet8.HangfireApi/DependencyInjection.cs
+++ DotNet8.HangfireApi/DependencyInjection.cs
@@ -1,72 +1,71 @@
-﻿namespace DotNet8.HangfireApi
+﻿namespace DotNet8.HangfireApi;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+
+    #region AddFeatures
+
+    public static IServiceCollection AddFeatures(this IServiceCollection services,WebApplicationBuilder builder)
     {
-
-        #region AddFeatures
-
-        public static IServiceCollection AddFeatures(this IServiceCollection services,WebApplicationBuilder builder)
-        {
-            return services
-                .AddDbContextService(builder)
-                .AddHangfireService(builder)
-                .AddRepositoryService()
-                .AddBusinessLogicService();
-        }
-
-        #endregion
-
-        #region AddDbContextService
-
-        private static IServiceCollection AddDbContextService(this IServiceCollection services,WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<AppDbContext>(
-                opt =>
-                {
-                    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-                },
-                ServiceLifetime.Transient
-            );
-            return services;
-        }
-
-        #endregion
-
-        #region AddRepositoryService
-
-        private static IServiceCollection AddRepositoryService(this IServiceCollection services)
-        {
-            return services.AddScoped<IBlogRepository, BlogRepository>();
-        }
-
-        #endregion
-
-        #region AddBusinessLogicService
-
-        private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
-        {
-            return services.AddScoped<BL_Blog>();
-        }
-
-        #endregion
-
-        #region AddHangfireService
-
-        private static IServiceCollection AddHangfireService(this IServiceCollection services,WebApplicationBuilder builder)
-        {
-            builder.Services.AddHangfire(opt =>
-            {
-                opt.UseSqlServerStorage(builder.Configuration.GetConnectionString("DbConnection"))
-                    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-                    .UseSimpleAssemblyNameTypeSerializer()
-                    .UseRecommendedSerializerSettings();
-            });
-
-            builder.Services.AddHangfireServer();
-            return services;
-        }
-
-        #endregion
-
+        return services
+            .AddDbContextService(builder)
+            .AddHangfireService(builder)
+            .AddRepositoryService()
+            .AddBusinessLogicService();
     }
+
+    #endregion
+
+    #region AddDbContextService
+
+    private static IServiceCollection AddDbContextService(this IServiceCollection services,WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(
+            opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            },
+            ServiceLifetime.Transient
+        );
+        return services;
+    }
+
+    #endregion
+
+    #region AddRepositoryService
+
+    private static IServiceCollection AddRepositoryService(this IServiceCollection services)
+    {
+        return services.AddScoped<IBlogRepository, BlogRepository>();
+    }
+
+    #endregion
+
+    #region AddBusinessLogicService
+
+    private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
+    {
+        return services.AddScoped<BL_Blog>();
+    }
+
+    #endregion
+
+    #region AddHangfireService
+
+    private static IServiceCollection AddHangfireService(this IServiceCollection services,WebApplicationBuilder builder)
+    {
+        builder.Services.AddHangfire(opt =>
+        {
+            opt.UseSqlServerStorage(builder.Configuration.GetConnectionString("DbConnection"))
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings();
+        });
+
+        builder.Services.AddHangfireServer();
+        return services;
+    }
+
+    #endregion
+
 }
