9a1220f85cf08f0fe44aea57cb022025ca0fe74d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 21:50:05 +0630

    Format Code

--- DotNet8.HangfireApi/Repositories/Blog/BlogRepository.cs
+++ DotNet8.HangfireApi/Repositories/Blog/BlogRepository.cs
@@ -1,45 +1,44 @@
-﻿namespace DotNet8.HangfireApi.Repositories.Blog
+﻿namespace DotNet8.HangfireApi.Repositories.Blog;
+
+public class BlogRepository : IBlogRepository
 {
-    public class BlogRepository : IBlogRepository
+    private readonly AppDbContext _appDbContext;
+
+    public BlogRepository(AppDbContext appDbContext)
     {
-        private readonly AppDbContext _appDbContext;
-
-        public BlogRepository(AppDbContext appDbContext)
-        {
-            _appDbContext = appDbContext;
-        }
-
-        public async Task<int> CreateBlog(BlogRequestModel requestModel)
-        {
-            try
-            {
-                await _appDbContext.TblBlogs.AddAsync(requestModel.Map());
-                return await _appDbContext.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        public async Task<BlogListResponseModel> GetBlogs()
-        {
-            try
-            {
-                var lst = await _appDbContext.TblBlogs
-                    .AsNoTracking()
-                    .OrderByDescending(x => x.BlogId)
-                    .ToListAsync();
-                return new BlogListResponseModel()
-                {
-                    DataLst = lst.Select(x => x.Map()).ToList()
-                };
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
+        _appDbContext = appDbContext;
     }
+
+    public async Task<int> CreateBlog(BlogRequestModel requestModel)
+    {
+        try
+        {
+            await _appDbContext.TblBlogs.AddAsync(requestModel.Map());
+            return await _appDbContext.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<BlogListResponseModel> GetBlogs()
+    {
+        try
+        {
+            var lst = await _appDbContext.TblBlogs
+                .AsNoTracking()
+                .OrderByDescending(x => x.BlogId)
+                .ToListAsync();
+            return new BlogListResponseModel()
+            {
+                DataLst = lst.Select(x => x.Map()).ToList()
+            };
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
 }
