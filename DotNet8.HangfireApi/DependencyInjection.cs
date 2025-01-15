91eb6fcb33309211c458ae6ee89a57a7439ac58b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 18:07:41 +0630

    Feat : Add AddDbContextService in DI

--- DotNet8.HangfireApi/DependencyInjection.cs
+++ DotNet8.HangfireApi/DependencyInjection.cs
@@ -1,6 +1,23 @@
-﻿namespace DotNet8.HangfireApi
+﻿using DotNet8.HangfireApi.Modelts;
+
+namespace DotNet8.HangfireApi
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
+        private static IServiceCollection AddDbContextService(
+            this IServiceCollection services,
+            WebApplicationBuilder builder
+        )
+        {
+            builder.Services.AddDbContext<AppDbContext>(
+                opt =>
+                {
+                    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+                },
+                ServiceLifetime.Transient
+            );
+
+            return services;
+        }
     }
 }
