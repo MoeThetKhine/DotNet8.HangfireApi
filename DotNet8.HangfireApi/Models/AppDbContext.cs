e07ec24b1d4cdac6eeb6ee8b110f973a80a08ad2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 14:41:01 +0630

    Feat : Add AppDbContext and Tables using Scaffold

---
+++ DotNet8.HangfireApi/Models/AppDbContext.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.HangfireApi.Modelts;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TaskCategory> TaskCategories { get; set; }
+
+    public virtual DbSet<TblBlog> TblBlogs { get; set; }
+
+    public virtual DbSet<ToDoList> ToDoLists { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.;Database=DotNetTrainingBatch5;User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskCategory>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__TaskCate__19093A2B739DC4B2");
+
+            entity.ToTable("TaskCategory");
+
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<TblBlog>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Tbl_Blog");
+
+            entity.Property(e => e.BlogAuthor).HasMaxLength(50);
+            entity.Property(e => e.BlogContent).HasMaxLength(50);
+            entity.Property(e => e.BlogId).ValueGeneratedOnAdd();
+            entity.Property(e => e.BlogTitle).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<ToDoList>(entity =>
+        {
+            entity.HasKey(e => e.TaskId).HasName("PK__ToDoList__7C6949D1F4B3258B");
+
+            entity.ToTable("ToDoList");
+
+            entity.Property(e => e.TaskId).HasColumnName("TaskID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CompletedDate).HasColumnType("datetime");
+            entity.Property(e => e.CreatedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Status)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.TaskDescription).HasColumnType("text");
+            entity.Property(e => e.TaskTitle)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Category).WithMany(p => p.ToDoLists)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__ToDoList__Catego__4E88ABD4");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
